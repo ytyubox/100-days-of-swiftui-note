047e777a0666891e601e573a6e2ecac9213242c5
Tsungyu Yu <ytyubox@gmail.com>
2019-10-26 18:19:03 +0800

    day 17 2/4, row and navigating to pickerPage

--- project 1 wesplit/WeSplit/WeSplit/ContentView.swift
+++ project 1 wesplit/WeSplit/WeSplit/ContentView.swift
@@ -7,32 +7,39 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    
-    // MARK: - Modal property
-    @State private var checkAmount = ""
-    @State private var numberOPerple = 2
-    @State private var tipPercentageIndex = 2
-    private let tipPercentages = [10, 15, 20, 25, 0]
-    
-    
-    var body: some View {
-       Form {
-            Section {
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
-            }
-
-            Section {
-                Text("$\(checkAmount)")
-            }
-        }
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct ContentView: View {
+    
+    // MARK: - Modal property
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentageIndex = 2
+    private let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
+    var body: some View {
+        NavigationView { // make Froms didSelectedRowAt to push new page
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }.navigationBarTitle("WeSplit")
+        }
+    }
+}
+
