52bdfe15b67717f0ff9d914b3db650e8f4fcd86b
Tsungyu Yu <ytyubox@gmail.com>
2019-10-26 13:58:29 +0800

    project init

---
+++ project 1 wesplit/README.md
@@ -0,0 +1,9 @@
+#  Day 16~17 Wesplit
+
+https://www.hackingwithswift.com/100/swiftui/16
+
+
+This project is a check-sharing app that calculates how to split a check based on the number of people and how much tip you want to leave. 
+
+
+## Part 1 
---
+++ project 1 wesplit/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by 游宗諭 on 2019/10/26.
+//  Copyright © 2019 ytyubox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
