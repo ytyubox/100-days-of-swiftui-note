e20ff2e04191011f07741e88e48aad7b6d69e49c
Tsungyu Yu <ytyubox@gmail.com>
2019-10-29 20:09:05 +0800

    day 20 6/6, showing alert

--- project2guesstheflag/GuessTheFlag/GuessTheFlag/SceneDelegate.swift
+++ project2guesstheflag/GuessTheFlag/GuessTheFlag/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let contentView = ContentView()
+        let contentView = AlertTry()// ContentView()
 
                // Use a UIHostingController as window root view controller.
                if let windowScene = scene as? UIWindowScene {
