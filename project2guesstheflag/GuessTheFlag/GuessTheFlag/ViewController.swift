6b9b8969d25a37b53f01e156efe51512c144691a
Tsungyu Yu <ytyubox@gmail.com>
2019-10-29 14:32:20 +0800

    project guess the flag init

---
+++ project2guesstheflag/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by 游宗諭 on 2019/10/29.
+//  Copyright © 2019 游宗諭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ project2guesstheflag/README.md
@@ -0,0 +1,3 @@
+#  Guess the flag
+resource: https://www.hackingwithswift.com/100/swiftui/20
+
