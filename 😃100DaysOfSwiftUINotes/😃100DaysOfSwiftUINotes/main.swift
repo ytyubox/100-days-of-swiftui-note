be08551f217f8c10f2c3891820aaf0ca8dcc29f8
Tsungyu Yu <ytyubox@gmail.com>
2020-04-11 15:54:48 +0800

    cancel workspace ~> cli project

---
+++ 😃100DaysOfSwiftUINotes/😃100DaysOfSwiftUINotes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  😃100DaysOfSwiftUINotes
+//
+//  Created by 游宗諭 on 2020/4/11.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
